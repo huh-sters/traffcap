aaa28197ef124bca07539cc1d4404f1ec2d8d7e6
Chris Read <centurix@gmail.com>
2023-05-21 00:02:42 +1000

    Inroads to testing sqlalchemy based repositories

--- src/traffcap/repositories/tests/test_endpoint_repository.py
+++ src/traffcap/repositories/tests/test_endpoint_repository.py
@@ -1,5 +1,6 @@
 import pytest
 from traffcap.repositories import EndpointRepository
+from alchemy_mock.mocking import AlchemyMagicMock
 """
 Tests for the endpoint repository
 """
@@ -7,10 +8,15 @@ Tests for the endpoint repository
 
 class TestEndpointRepository:
     @pytest.mark.asyncio
-    async def test_it_returns_one_endpoint_by_id(self):
+    async def test_it_returns_one_endpoint_by_id(self, mocker):
         """
         Mock the Repository.session() to return the UnifiedAlchemyMagicMock
+        TODO: Mock the sqlalchemy async session somehow
         """
+        mocker.patch(
+            "traffcap.repositories.Repository.session.__aenter__",
+            return_value=AlchemyMagicMock()
+        )
         results = await EndpointRepository.get_endpoint_by_id(1)
 
         assert results is not None
