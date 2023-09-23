51c626661c85d8ed915f54c6f11837880431ac63
Chris Read <centurix@gmail.com>
2023-09-23 18:25:38 +1000

    Moved openapi stuff to pydanja

--- src/traffcap/server.py
+++ src/traffcap/server.py
@@ -17,7 +17,7 @@ from starlette.requests import Request
 from fastapi.responses import JSONResponse
 from traffcap.repositories import Repository
 from fastapi.openapi.utils import get_openapi
-import dpath
+from pydanja import danja_openapi
 """
 Application bootstrap
 """
@@ -33,104 +33,16 @@ def custom_openapi():
         return app.openapi_schema
 
     openapi_schema = get_openapi(
-        title="Custom title",
+        title="FastAPI",
         version="2.5.0",
-        summary="This is a very custom OpenAPI schema",
-        description="Here's a longer description of the custom **OpenAPI** schema",
+        summary="FastAPI",
+        description="FastAPI",
         routes=app.routes,
     )
 
-    # Get all referenced schemas
-    ref_schemas = set([
-        result[1][21:]
-        for result in dpath.search(openapi_schema, "paths/**/$ref", yielded=True)
-    ])
+    app.openapi_schema = danja_openapi(openapi_schema)
 
-    # Resolve all schema references
-    ex_schemas = {}
-    for key, value in openapi_schema.get("components", {}).get("schemas", {}).items():
-        # Flatten and add to the schemas
-        for result in dpath.search(value, "/**/$ref", yielded=True):
-            parent, _, _ = result[0].rpartition("/")
-            dpath.set(value, parent, dpath.get(openapi_schema, result[1][1:]))
-        
-        if key in ref_schemas:
-            ex_schemas[key] = value
-
-    # Rename the DANJA objects
-    # DANJAErrorList -> Error_List
-    # DANJAResourceList_model -> model_List
-    # DANJAResource_model -> model
-    rn_schemas = {}
-    for key in ex_schemas:
-        old_key = f"#/components/schemas/{key}"
-        # Find the key in the paths and rename the key and reference
-        if key.startswith("DANJAResource_"):
-            new_key = key.replace("DANJAResource_", "")
-            # Find all path references to the old key
-            for result in dpath.search(
-                openapi_schema,
-                "paths|**|$ref",
-                yielded=True,
-                separator="|"
-            ):
-                if result[1] == old_key:
-                    # Rename to the new key
-                    dpath.set(
-                        openapi_schema,
-                        result[0],
-                        f"#/components/schemas/{new_key[:-1]}",
-                        separator="|"
-                    )
-
-            rn_schemas[new_key[:-1]] = ex_schemas[key]
-        elif key.startswith("DANJAResourceList_"):
-            new_key = key.replace("DANJAResourceList_", "")
-            # Find all path references to the old key
-            for result in dpath.search(
-                openapi_schema,
-                "paths|**|$ref",
-                yielded=True,
-                separator="|"
-            ):
-                if result[1] == old_key:
-                    # Rename to the new key
-                    dpath.set(
-                        openapi_schema,
-                        result[0],
-                        f"#/components/schemas/{new_key}List",
-                        separator="|"
-                    )
-
-            rn_schemas[f"{new_key}List"] = ex_schemas[key]
-        elif key == "DANJAErrorList":
-            new_key = "Error_List"
-            # Find all path references to the old key
-            for result in dpath.search(
-                openapi_schema,
-                "paths|**|$ref",
-                yielded=True,
-                separator="|"
-            ):
-                if result[1] == old_key:
-                    # Rename to the new key
-                    dpath.set(
-                        openapi_schema,
-                        result[0],
-                        f"#/components/schemas/{new_key}",
-                        separator="|"
-                    )
-
-            rn_schemas[new_key] = ex_schemas[key]
-        else:
-            # Don't rename
-            rn_schemas[key] = ex_schemas[key]
-
-    openapi_schema["components"]["schemas"] = rn_schemas
-
-    app.openapi_schema = openapi_schema
     return app.openapi_schema
-
 
 app.openapi = custom_openapi
 
